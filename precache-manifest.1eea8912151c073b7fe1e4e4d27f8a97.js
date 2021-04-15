2f674ad6af540f339c5c40b4e164a64f564f75f1
Oskar <dyngusoskar@gmail.com>
2021-04-15 14:02:26 +0200

    test

---
+++ precache-manifest.1eea8912151c073b7fe1e4e4d27f8a97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea3e8eafb04d3694c109c244e0e05bb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d245fec7295d3e614517",
+    "url": "/static/css/main.ca042618.chunk.css"
+  },
+  {
+    "revision": "2776555b1cebe4e0fa34",
+    "url": "/static/js/2.783533e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.783533e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d245fec7295d3e614517",
+    "url": "/static/js/main.f8112e66.chunk.js"
+  },
+  {
+    "revision": "e87075461662cf05c8fd",
+    "url": "/static/js/runtime-main.f6926e12.js"
+  },
+  {
+    "revision": "eecf09b4d7c208d19170e123ceee2187",
+    "url": "/static/media/babyblue6.eecf09b4.png"
+  }
+]);
